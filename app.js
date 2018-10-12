3a46af5edeff29c6e4564e7e69d1a09f30ec7466
Anikesh Patel <anikeshpatel4@gmail.com>
2018-10-12 14:45:23 +0530

    Init project

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -11,10 +10,9 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+app.listen(process.env.PORT || 3000);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/', (req, res) => {
+  // res.render('index', { title: 'Express' });
+    console.log('Its working');
+    res.json({
+        name: 'Anikesh Patel',
+        email: 'anikeshpatel4@gmail.com',
+        passwd: '***********',
+        skills: [
+            'Java',
+            'Python',
+            'JavaScript'
+        ]
+    });
 });
 
 module.exports = router;
